560f437f050b1314fa113274183088a6cc6cc84c
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-06-24 16:24:52 +0530

    Dependencies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { ScrollTrigger, SplitText } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
- return (
-  <div >
-    hello hi there
-  </div>
- )
+  return (
+    <div >
+      hello hi there how are you
+    </div>
+  )
 }
 
 export default App
